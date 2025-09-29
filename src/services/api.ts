7b136ccf079263bf76244d80326be91c7444038d
Letto <karvatwellinton@hotmail.com>
2025-09-28 21:51:27 -0300

    go

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ import axios, { AxiosError } from "axios";
  * BASE URL
  * ============================= */
 const ENV_BASE = import.meta.env?.VITE_API_URL;
-const FALLBACK_NGROK = "https://4151984f1918.ngrok-free.app"; // opcional
+const FALLBACK_NGROK = "https://a5e2abe24ab4.ngrok-free.app"; // opcional
 const baseURL =
   (ENV_BASE && ENV_BASE.trim() !== "" ? ENV_BASE : FALLBACK_NGROK) ||
   "http://localhost:5000";
